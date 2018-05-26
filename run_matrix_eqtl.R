7151b6ae33db7f5aadfe22db7b5292e18e18b317
Liam Flinn Spurr <30680009+lfspurr@users.noreply.github.com>
2018-05-26 19:48:57 -0400

    Add files via upload

---
+++ run_matrix_eqtl.R
@@ -0,0 +1,109 @@
+# LIAM SPURR (based off of MatrixEQTL sample commands)
+# HORVATH LAB
+# 24MAY18
+# RUN_MATRIX_EQTL.R
+
+# install missing required packages and load packages
+load_package <- function(x) {
+  if (!require(x, character.only = TRUE)) {
+    install.packages(x, dep = TRUE)
+    if(!require(x, character.only = TRUE)) stop(paste0("Package: ", x, " not found"))
+  }
+}
+
+load_package("MatrixEQTL")
+
+# get arguments from the command line
+args = commandArgs(trailingOnly=TRUE)
+
+
+# Linear model to use, modelANOVA, modelLINEAR, or modelLINEAR_CROSS
+useModel = modelLINEAR;
+
+# Genotype and gene location file names
+SNP_file_name = args[1]
+snps_location_file_name = args[2]
+
+# Gene expression file name
+expression_file_name = args[3]
+gene_location_file_name = args[4]
+
+# Covariates file name
+# Covariates can be added in by initiating the variable to args[n], incrementing each subsequent args[m] = args[m + 1]
+covariates_file_name = character()
+
+# Output file name
+output_file_name_cis = args[5]
+output_file_name_tra = args[6]
+
+# Only associations significant at this level will be saved
+pvOutputThreshold_cis = 1e-5;
+pvOutputThreshold_tra = 1e-5;
+
+# Error covariance matrix
+# Set to numeric() for identity.
+errorCovariance = numeric();
+
+# Distance for local gene-SNP pairs
+cisDist = 1e6;
+
+
+## Load genotype data
+
+snps = SlicedData$new();
+snps$fileDelimiter = "\t";      # the TAB character
+snps$fileOmitCharacters = "NA"; # denote missing values;
+snps$fileSkipRows = 1;          # one row of column labels
+snps$fileSkipColumns = 1;       # one column of row labels
+snps$fileSliceSize = 2000;      # read file in slices of 2,000 rows
+snps$LoadFile(SNP_file_name);
+
+## Load gene expression data
+
+gene = SlicedData$new();
+gene$fileDelimiter = "\t";      # the TAB character
+gene$fileOmitCharacters = "NA"; # denote missing values;
+gene$fileSkipRows = 1;          # one row of column labels
+gene$fileSkipColumns = 1;       # one column of row labels
+gene$fileSliceSize = 2000;      # read file in slices of 2,000 rows
+gene$LoadFile(expression_file_name);
+
+## Load covariates
+
+cvrt = SlicedData$new();
+cvrt$fileDelimiter = "\t";      # the TAB character
+cvrt$fileOmitCharacters = "NA"; # denote missing values;
+cvrt$fileSkipRows = 1;          # one row of column labels
+cvrt$fileSkipColumns = 1;       # one column of row labels
+if(length(covariates_file_name) > 0) {
+  cvrt$LoadFile(covariates_file_name);
+}
+
+## Run the analysis
+snpspos = read.table(snps_location_file_name, header = TRUE, stringsAsFactors = FALSE)
+genepos = read.table(gene_location_file_name, header = TRUE, stringsAsFactors = FALSE)
+
+me = Matrix_eQTL_main(
+  snps = snps, 
+  gene = gene, 
+  cvrt = cvrt,
+  output_file_name = output_file_name_tra,
+  pvOutputThreshold  = pvOutputThreshold_tra,
+  useModel = useModel, 
+  errorCovariance = errorCovariance, 
+  verbose = TRUE, 
+  output_file_name.cis = output_file_name_cis,
+  pvOutputThreshold.cis = pvOutputThreshold_cis,
+  snpspos = snpspos, 
+  genepos = genepos,
+  cisDist = cisDist,
+  pvalue.hist = "qqplot",
+  min.pv.by.genesnp = FALSE,
+  noFDRsaveMemory = FALSE);
+
+# Produce and save the qq plot of the p-values
+tiff(filename = args[7])
+plot(me)
+dev.off()
+
+cat('Analysis done in: ', me$time.in.sec, ' seconds', '\n')
