76ba12e8df52d3c2b253179ee35b56066f9f7cca
Liam Flinn Spurr <30680009+lfspurr@users.noreply.github.com>
2019-09-09 13:57:53 -0400

    Update build_gene-exp_matrix.R

--- build_gene-exp_matrix.R
+++ build_gene-exp_matrix.R
@@ -1,7 +1,6 @@
-# LIAM SPURR
-# HORVATH LAB
-# 24MAY18
 # BUILD_GENE-EXP_MATRIX.R
+# LAST UPDATED BY LIAM FLINN SPURR ON SEPTEMBER 9, 2019
+# THIS SCRIPT BUILDS THE GENE EXPRESSION MATRIX FOR REQTL ANALYSIS
 
 # install missing required packages and load packages
 load_package <- function(x) {
@@ -13,53 +12,61 @@ load_package <- function(x) {
 
 load_package("dplyr"); load_package("tidyr"); load_package("data.table");
 
-# get the command line arguments
-args = commandArgs(trailingOnly = T)
+# take arguments from the command line
+args <- commandArgs(trailingOnly = TRUE)
 
-# identify the location of the stringtie gene expression files
-gene_abund_path = args[1]
-abund_files = list.files(path = gene_abund_path)
-
-# create an empty data frame to hold the combined gene expresion files
-df = data.frame()
+# identify the location of the readcount files
+# you may wish to modify the pattern to match the naming conventions of your samples
+exp_path <- args[1]
+exp_files <- list.files(path = exp_path, pattern = 'gene_abund')
 
 # load in the readcounts
-for (file in abund_files) {
-  # create a temporary file for each individual gene expression file
-  temp = fread(paste0(gene_abund_path, file))
-  
-  # select the necessary columns and create the sample column
-  temp = temp %>% select(1,2,3,5,6,9) %>% 
-    mutate(sample = !!file) %>%
+df <- bind_rows(lapply(exp_files, function(f) {
+  # select the necessary columns, fix sample name formatting, and remove genes without an annotation
+  fread(paste0(exp_path, f)) %>% 
+    select(1,2,3,5,6,9) %>% 
+    mutate(sample = gsub(x = f, pattern = '_.*$', replacement = '')) %>%
     filter(`Gene Name` != '-')
-  
-  # combine all of the files together
-  df = rbind(df, temp)
-}
+}))
 
-# remove spaces from column name
-df = df %>% rename(GeneID = `Gene ID`)
+# create gene coordinate table
+df_loc <- df %>% select(1,3,4,5) %>% 
+  distinct() %>% 
+  mutate(Reference = paste0('chr', Reference)) %>% 
+  rename(GeneID = `Gene ID`)
 
-# create gene location file
-df_loc = df %>% select(1,3,4,5) %>% distinct() %>% mutate(Reference = paste0('chr', Reference))
+df <- df %>% select(1,2,6,7)
 
-# remove duplicate identifiers
-df = df %>% select(sample, TPM, `Gene ID`) %>% group_by(sample, `Gene ID`) %>% filter(row_number() == 1)
+# get number of samples
+num_samples <- length(levels(factor(df$sample)))
 
-# spread the file into a matrix
-df = df %>% spread(sample, TPM)
+# retain only genes that are expressed at TPM >=1 in at least 20% of the samples
+df <- df %>% group_by(`Gene ID`) %>% mutate(count_zero = length(which(TPM < 1)),
+                                             perc_zero = count_zero / num_samples)
 
-# transform gene expression values into quantile normalized form
-df_w = df[-1]
+# remove duplicate entries for each gene if necessary
+df <- df %>% filter(perc_zero < 0.8) %>% 
+  select(sample, TPM, `Gene ID`) %>% 
+  group_by(sample, `Gene ID`) %>% 
+  filter(row_number() == 1) %>%
+  spread(sample, TPM) %>% 
+  rename(GeneID = `Gene ID`)
+
+
+# quantile normalize gene expression values
+# (code adapted from MatrixEQTL sample code)
+df_w <- df[-1]
+
 for(gene in 1:nrow(df_w)) {
   mat = df_w[gene,]
   mat = apply(mat, 1, rank, ties.method = "average")
   mat = qnorm(mat / (ncol(df_w) + 1))
   df_w[gene,] = mat
 }
-df_w$gene_id = df$GeneID
-df_w = df_w %>% select(gene_id, everything())
+df_w$gene_id <- df$GeneID
+df_w <- df_w %>% select(gene_id, everything())
 
-# write outputs to files
-write.table(df_w, args[2], quote = F, row.names = F, sep = '\t')
-write.table(df_loc, args[3], quote = F, row.names = F, sep = '\t')
+# write outputs
+output_prefix <- args[2]
+write.table(df_w, paste0(output_prefix, '_gene-exp_norm_matrix.txt'), quote = F, row.names = F, sep = '\t')
+write.table(df_loc, paste0(output_prefix, '_gene-exp-loc_matrix.txt'), quote = F, row.names = F, sep = '\t')
