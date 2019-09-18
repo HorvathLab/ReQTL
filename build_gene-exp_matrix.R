9685923f1a03ba5711b82189f5db5b3bd4b7e568
lfspurr <LiamF_Spurr@dfci.harvard.edu>
2019-09-18 10:18:27 -0400

    fix overwritten gene-exp matrix script

--- build_gene-exp_matrix.R
+++ build_gene-exp_matrix.R
@@ -1,7 +1,6 @@
-# RUN_MATRIX_REQTL.R
+# BUILD_GENE-EXP_MATRIX.R
 # LAST UPDATED BY LIAM FLINN SPURR ON SEPTEMBER 9, 2019
-# THIS SCRIPT IS BASED OFF OF THE MATRIX EQTL SAMPLE CODE
-# ORIGINAL CODE AVAILABLE AT http://www.bios.unc.edu/research/genomic_software/Matrix_eQTL/runit.html#sample
+# THIS SCRIPT BUILDS THE GENE EXPRESSION MATRIX FOR REQTL ANALYSIS
 
 # install missing required packages and load packages
 load_package <- function(x) {
@@ -11,137 +10,66 @@ load_package <- function(x) {
   }
 }
 
-load_package("MatrixEQTL"); load_package("tidyverse")
+load_package("dplyr"); load_package("tidyverse")
 
-# get arguments from the command line
+# take arguments from the command line
 args <- commandArgs(trailingOnly = TRUE)
 
-# manages command line arguments
-handle_command_args <- function(args) {
-  # make sure all flags are paired
-  if(length(args) %% 2 != 0) stop("Command line arguments supplied incorrectly!")
-  
-  # load the flags into a "dictionary"
-  arg_df <- data.frame(cbind(flag = args[seq(1, length(args), by = 2)], value = args[seq(2, length(args), by = 2)])) %>%
-    mutate_all(as.character)
-  
-  # Linear model to use, modelANOVA, modelLINEAR, or modelLINEAR_CROSS
-  useModel <<- modelLINEAR
-  
-  # Genotype and gene location file names
-  SNV_file_name <<- arg_df$value[arg_df$flag == "-s"]
-  snvs_location_file_name <<- arg_df$value[arg_df$flag == "-sl"]
-  
-  # Gene expression file name
-  expression_file_name <<- arg_df$value[arg_df$flag == "-g"]
-  gene_location_file_name <<- arg_df$value[arg_df$flag == "-gl"]
-  
-  # Covariates file name
-  covariates_file_name <<- ifelse(length(arg_df$value[arg_df$flag == "-c"]) > 0, arg_df$value[arg_df$flag == "-c"], "")
-  
-  # Whether to split into cis and trans
-  split_cis_trans <<- arg_df$value[arg_df$flag == "-ct"]
-  
-  # Error covariance matrix
-  # Set to numeric() for identity.
-  errorCovariance <<- numeric()
-  
-  # Filename for qqplot
-  qqplot_filename <<- paste0(arg_df$value[arg_df$flag == "-qq"], ".tiff")
-  
-  if(split_cis_trans == "T") {
-    # Output file name
-    output_file_name_cis <<- paste0(arg_df$value[arg_df$flag == "-cis"], ".txt")
-    output_file_name_tra <<- paste0(arg_df$value[arg_df$flag == "-tr"], ".txt")
-    
-    # Only associations significant at this level will be saved
-    pvOutputThreshold_cis <<- as.numeric(arg_df$value[arg_df$flag == "-pcis"])
-    pvOutputThreshold_tra <<- as.numeric(arg_df$value[arg_df$flag == "-ptr"])
-    
-    # Distance for local gene-SNV pairs
-    cisDist <<- 1e6
-    
-  } else {
-    # Output file name
-    output_file_name <<- paste0(arg_df$value[arg_df$flag == "-o"], ".txt")
-    
-    # Only associations significant at this level will be saved
-    pvOutputThreshold <<- as.numeric(arg_df$value[arg_df$flag == "-p"])
-  }
+# identify the location of the readcount files
+# you may wish to modify the pattern to match the naming conventions of your samples
+# the pattern identified here is the suffix of the samples that will later be removed to get the sample name 
+exp_path <- args[1]
+pattern <- "_gene_abund.tab"
+exp_files <- list.files(path = exp_path, pattern = pattern)
+
+# load in the readcounts
+df <- bind_rows(lapply(exp_files, function(f) {
+  # select the necessary columns, including gene position and TPM value
+  # fix sample name formatting, and remove genes without an annotation
+  fread(paste0(exp_path, f)) %>% 
+    select(`Gene ID`,`Gene Name`, Reference, Start, End, TPM) %>% 
+    mutate(sample = gsub(x = f, pattern = pattern, replacement = '')) %>% # remove sample name suffix specified in "pattern"
+    filter(`Gene Name` != '-')
+}))
+
+# create gene coordinate table
+df_loc <- df %>% select(`Gene ID`, Reference, Start, End) %>% 
+  distinct() %>% 
+  mutate(Reference = paste0('chr', Reference)) %>% 
+  rename(GeneID = `Gene ID`)
+
+df <- df %>% select(`Gene ID`, `Gene Name`, TPM, sample)
+
+# get number of samples
+num_samples <- length(levels(factor(df$sample)))
+
+# retain only genes that are expressed at TPM >=1 in at least 20% of the samples
+df <- df %>% group_by(`Gene ID`) %>% mutate(count_zero = length(which(TPM < 1)),
+                                             perc_zero = count_zero / num_samples)
+
+# remove duplicate entries for each gene if necessary
+df <- df %>% filter(perc_zero < 0.8) %>% 
+  select(sample, TPM, `Gene ID`) %>% 
+  group_by(sample, `Gene ID`) %>% 
+  filter(row_number() == 1) %>%
+  spread(sample, TPM) %>% 
+  rename(GeneID = `Gene ID`)
+
+
+# quantile normalize gene expression values
+# (code adapted from MatrixEQTL sample code)
+df_w <- df[-1]
+
+for(gene in 1:nrow(df_w)) {
+  mat = df_w[gene,]
+  mat = apply(mat, 1, rank, ties.method = "average")
+  mat = qnorm(mat / (ncol(df_w) + 1))
+  df_w[gene,] = mat
 }
+df_w$gene_id <- df$GeneID
+df_w <- df_w %>% select(gene_id, everything())
 
-handle_command_args(args)
-
-## Load genotype data
-snvs <- SlicedData$new()
-snvs$fileDelimiter = "\t"      # the TAB character
-snvs$fileOmitCharacters = "NA" # denote missing values
-snvs$fileSkipRows = 1          # one row of column labels
-snvs$fileSkipColumns = 1       # one column of row labels
-snvs$fileSliceSize = 2000      # read file in slices of 2,000 rows
-snvs$LoadFile(SNV_file_name)
-
-## Load gene expression data
-
-gene <- SlicedData$new()
-gene$fileDelimiter = "\t"      # the TAB character
-gene$fileOmitCharacters = "NA" # denote missing values
-gene$fileSkipRows = 1          # one row of column labels
-gene$fileSkipColumns = 1       # one column of row labels
-gene$fileSliceSize = 2000      # read file in slices of 2,000 rows
-gene$LoadFile(expression_file_name)
-
-## Load covariates
-cvrt <- SlicedData$new()
-cvrt$fileDelimiter = "\t"      # the TAB character
-cvrt$fileOmitCharacters = "NA" # denote missing values
-cvrt$fileSkipRows = 1          # one row of column labels
-cvrt$fileSkipColumns = 1       # one column of row labels
-if(length(covariates_file_name) > 1) {
-  cvrt$LoadFile(covariates_file_name)
-}
-
-## Run the analysis
-snvspos <- read.table(snvs_location_file_name, header = TRUE, stringsAsFactors = FALSE)
-genepos <- read.table(gene_location_file_name, header = TRUE, stringsAsFactors = FALSE)
-
-if(split_cis_trans == "T") {
-  me <- Matrix_eQTL_main(
-    snps = snvs, 
-    gene = gene, 
-    cvrt = cvrt,
-    output_file_name = output_file_name_tra,
-    pvOutputThreshold  = pvOutputThreshold_tra,
-    useModel = useModel, 
-    errorCovariance = errorCovariance, 
-    verbose = TRUE, 
-    output_file_name.cis = output_file_name_cis,
-    pvOutputThreshold.cis = pvOutputThreshold_cis,
-    snpspos = snvspos, 
-    genepos = genepos,
-    cisDist = cisDist,
-    pvalue.hist = "qqplot",
-    min.pv.by.genesnp = FALSE,
-    noFDRsaveMemory = FALSE)
-} else {
-  me <- Matrix_eQTL_main(
-    snps = snvs, 
-    gene = gene, 
-    cvrt = cvrt,
-    output_file_name = output_file_name,
-    useModel = useModel, 
-    errorCovariance = errorCovariance, 
-    verbose = TRUE, 
-    pvOutputThreshold= pvOutputThreshold,
-    snpspos = snvspos, 
-    genepos = genepos,
-    pvalue.hist = "qqplot",
-    min.pv.by.genesnp = FALSE,
-    noFDRsaveMemory = FALSE);
-}
-
-tiff(filename = qqplot_filename)
-plot(me)
-dev.off()
-
-cat('Analysis done in: ', me$time.in.sec, ' seconds', '\n')
+# write outputs
+output_prefix <- args[2]
+write.table(df_w, paste0(output_prefix, '_gene-exp_matrix.txt'), quote = F, row.names = F, sep = '\t')
+write.table(df_loc, paste0(output_prefix, '_gene-exp-loc_matrix.txt'), quote = F, row.names = F, sep = '\t')
